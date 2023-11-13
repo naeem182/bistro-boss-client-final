95c50a9266dda7c9f603d7b5234d8fca8572f580
naeem <naeemislamjr182@gmail.com>
2023-11-14 02:19:00 +0600

    auth createuser,sign in,signout

--- src/Provider/Authprovider.jsx
+++ src/Provider/Authprovider.jsx
@@ -1,14 +1,41 @@
-import { createContext, useState } from "react"
+import { createUserWithEmailAndPassword, onAuthStateChanged, signInWithEmailAndPassword, signOut } from "firebase/auth"
+import { createContext, useEffect, useState } from "react"
+import auth from "../firebase/firebase.config"
 
 export const AuthContex = createContext(null)
 const Authprovider = ({ children }) => {
     const [user, setuser] = useState(null)
     const [loading, setloading] = useState(true)
 
+    const createuser = (email, password) => {
+        setloading(true);
+        return createUserWithEmailAndPassword(auth, email, password)
+    }
 
+    const signin = (email, password) => {
+        setloading(true);
+        return signInWithEmailAndPassword(auth, email, password)
+    }
+    const logout = () => {
+        setloading(true);
+        return signOut(auth)
+    }
+
+    //user ase ki na
+    useEffect(() => {
+        const unsubscribe = onAuthStateChanged(auth, currentuser => {
+            setuser(currentuser);
+            console.log("user in the currnt state", currentuser);
+            setloading(falseals)
+        })
+        return unsubscribe;
+    }, [])
     const authinfo = {
         user,
         loading,
+        createuser,
+        signin,
+        logout,
 
     }
 
