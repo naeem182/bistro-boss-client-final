b1fccdadf6995cf1e5c2318479c7f2c51297a33c
naeem <naeemislamjr182@gmail.com>
2023-11-17 03:31:16 +0600

    handle delete from cart item

--- src/Dashboard/cart/Cart.jsx
+++ src/Dashboard/cart/Cart.jsx
@@ -1,24 +1,56 @@
+import Swal from "sweetalert2";
 import useCart from "../../hooks/useCart";
+import useAxiosSecure from "../../hooks/useAxiosSecure";
+import { FaTrashAlt } from "react-icons/fa";
 
 const Cart = () => {
-    const [cart] = useCart();
-    const totalPrice = cart.reduce((total, item) => total + item.price, 0)
+    const [cart, refetch] = useCart();
+    const totalPrice = cart.reduce((total, item) => total + item.price, 0);
+    const axiosSecure = useAxiosSecure();
+
+
+    const handleDelete = id => {
+        Swal.fire({
+            title: "Are you sure?",
+            text: "You won't be able to revert this!",
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#3085d6",
+            cancelButtonColor: "#d33",
+            confirmButtonText: "Yes, delete it!"
+        }).then((result) => {
+            if (result.isConfirmed) {
+
+                axiosSecure.delete(`/carts/${id}`)
+                    .then(res => {
+                        if (res.data.deletedCount > 0) {
+                            refetch();
+                            Swal.fire({
+                                title: "Deleted!",
+                                text: "Your file has been deleted.",
+                                icon: "success"
+                            });
+                        }
+                    })
+            }
+        });
+    }
+
     return (
         <div>
-            <h2 className="text-6xl">My Cart</h2>
-            <div className="flex justify-evenly">
+            <div className="flex justify-evenly mb-8">
                 <h2 className="text-4xl">Items: {cart.length}</h2>
                 <h2 className="text-4xl">Total Price: {totalPrice}</h2>
                 <button className="btn btn-primary">Pay</button>
 
             </div>
             <div className="overflow-x-auto">
-                <table className="table">
+                <table className="table  w-full">
                     {/* head */}
                     <thead>
                         <tr>
                             <th>
-                                No
+                                #
                             </th>
                             <th>Image</th>
                             <th>Name</th>
@@ -29,9 +61,9 @@ const Cart = () => {
                     <tbody>
                         {
                             cart.map((item, index) => <tr key={item._id}>
-                                <td>
+                                <th>
                                     {index + 1}
-                                </td>
+                                </th>
                                 <td>
                                     <div className="flex items-center gap-3">
                                         <div className="avatar">
@@ -39,17 +71,18 @@ const Cart = () => {
                                                 <img src={item.image} alt="Avatar Tailwind CSS Component" />
                                             </div>
                                         </div>
-
                                     </div>
                                 </td>
                                 <td>
                                     {item.name}
                                 </td>
-                                <td>
-                                    {item.price}
-                                </td>
+                                <td>${item.price}</td>
                                 <th>
-                                    <button className="btn btn-ghost btn-xs">details</button>
+                                    <button
+                                        onClick={() => handleDelete(item._id)}
+                                        className="btn btn-ghost btn-lg">
+                                        <FaTrashAlt className="text-red-600"></FaTrashAlt>
+                                    </button>
                                 </th>
                             </tr>)
                         }
