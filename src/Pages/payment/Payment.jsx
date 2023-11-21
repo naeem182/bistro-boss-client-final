5d5b844fec1563832cd755eeb9d6d17d76674180
naeem <naeemislamjr182@gmail.com>
2023-11-21 14:54:26 +0600

    checkout form and publish key

--- src/Pages/payment/Payment.jsx
+++ src/Pages/payment/Payment.jsx
@@ -2,16 +2,17 @@ import { loadStripe } from "@stripe/stripe-js";
 
 import { Elements } from "@stripe/react-stripe-js";
 import HEadingTittle from "../Shared/HEadingTittle";
+import CheckoutForm from "./CheckoutForm";
 
 // TODO: add publishable key
-const stripePromise = loadStripe('');
+const stripePromise = loadStripe(import.meta.env.VITE_Payment_Gateway_PK);
 const Payment = () => {
     return (
         <div>
             <HEadingTittle headingeading="Payment" subHeading="Please pay to eat"></HEadingTittle>
             <div>
                 <Elements stripe={stripePromise}>
-
+                    <CheckoutForm></CheckoutForm>
                 </Elements>
             </div>
         </div>
